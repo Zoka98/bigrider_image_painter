e3eac3460aa4b6151c6e5b471f9711f17d1448ba
Zoka98 <109471773+Zoka98@users.noreply.github.com>
2025-04-05 20:46:32 +0200

    Update bigrider_image_painter.dart

--- lib/bigrider_image_painter.dart
+++ lib/bigrider_image_painter.dart
@@ -1,6 +1,6 @@
 library bigrider_image_painter;
 
-export 'src/image_painter.dart';
+export 'src/_image_painter.dart';
 export 'src/_paint_over_image.dart';
 export 'src/controller.dart';
 export 'src/delegates/text_delegate.dart';
